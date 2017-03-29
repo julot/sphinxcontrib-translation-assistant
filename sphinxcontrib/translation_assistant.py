9514a8455a65b57634efe9c8e71a4d1c4bd0a9ba
Julot <andy.julot@gmail.com>
2017-03-29 13:16:43 +0700

    Improve nodes hierarchy

--- sphinxcontrib/translation_assistant.py
+++ sphinxcontrib/translation_assistant.py
@@ -1,3 +1,4 @@
+from collections import deque
 from itertools import zip_longest
 from docutils import nodes
 from docutils.parsers.rst import directives
@@ -23,7 +24,7 @@ def split(text):
 
 
 def merge(raws, translations):
-    data = []
+    data = deque()
 
     for raw, translation in zip_longest(raws, translations, fillvalue=''):
         prefix = raw[0]
@@ -43,33 +44,47 @@ class Directive(BaseDirective):
     final_argument_whitespace = True
     has_content = False
 
+    option_spec = {'no-title': directives.flag}
+
     def run(self):
         env = self.state.document.settings.env
 
         rel_path, path = env.relfn2path(directives.path(self.arguments[0]))
         env.note_dependency(rel_path)
 
-        print(rel_path)
-        print(path)
+        no_title = 'no-title' in self.options
 
         with open(path, encoding='utf-8') as f:
             text = f.read()
 
         lines = parse(text=text)
 
-        data = []
+        section = None
+
+        if not no_title:
+            line = lines.popleft()
+            title = ' - '.join([line[0], line[1]])
+
+            section = nodes.section(ids=[title])
+            section.append(nodes.title(text=title))
+            lines.popleft()
+
+        contents = []
 
         for line in lines:
             container = nodes.container(classes=['raw'])
             container.append(nodes.generated(text=line[0] or '　'))
-            data.append(container)
+            contents.append(container)
 
             if line[1]:
                 container = nodes.container(classes=['translated'])
                 container.append(nodes.generated(text=line[1]))
-                data.append(container)
+                contents.append(container)
 
-        return data
+        if not no_title:
+            section.extend(contents)
+
+        return [section] if section else contents
 
 
 def setup(app):
